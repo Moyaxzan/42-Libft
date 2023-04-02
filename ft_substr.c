424fd6a691e5664ee0e2cceb648e649f2f9d776a
Tao <tao.spa2003@gmail.com>
2023-04-02 19:30:42 +0200

    ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taospa <taospa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/02 18:40:45 by taospa            #+#    #+#             */
+/*   Updated: 2023/04/02 19:29:15 by taospa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//veriff if negative start or len ?
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char				*substring;
+	unsigned int		i;
+
+	i = start;
+	while (i < start + len && s[i])
+		i++;
+	substring = malloc(sizeof(char) * i);
+	if (!substring)
+		return (0x0);
+	i = start;
+	while (i < start + len && s[i])
+	{
+		substring[i - start] = s[i];
+		i++;
+	}
+	return (substring);
+}
