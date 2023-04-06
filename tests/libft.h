4be0b31b0d9d4dc8a21cfe6150512c9e41c4d004
Tao <tao.spa2003@gmail.com>
2023-04-07 00:46:38 +0200

    adding

---
+++ tests/libft.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsaint-p <tsaint-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 15:16:20 by tsaint-p          #+#    #+#             */
+/*   Updated: 2023/03/21 15:16:20 by tsaint-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+
+int		ft_atoi(char *theString);
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(char character);
+int		ft_isalpha(char character);
+int		ft_isascii(int character);
+int		ft_isdigit(char character);
+int		ft_isprint(int character);
+char	*ft_itoa(int n);
+void	*ft_memchr(const void *memoryBlock, int searchedChar, size_t size);
+int		ft_memcmp(const void *pointer1, const void *pointer2, size_t size);
+void	*ft_memcpy(void *destination, const void *source, size_t size);
+void	*ft_memmove(void *destination, const void *source, size_t size);
+void	*ft_memset(void *pointer, int value, size_t count);
+char	**ft_split(char const *src, char separator);
+char	*ft_strchr(char *string, int searchedChar);
+char	*ft_strjoin(int size, char **strs, char *sep);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
+size_t	ft_strlen(const char *string);
+int		ft_strncmp(const char *first, const char *second, size_t length);
+char	*ft_strnstr(char *big, const char *little, size_t len);
+char	*ft_strrchr(char *string, int searchedChar);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
---
+++ tests/main.c
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 23:42:39 by marvin            #+#    #+#             */
+/*   Updated: 2023/03/21 23:42:39 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "libft.h"
+
+int	main(void)
+{
+	//isalnum
+	printf("isalnum :\n");
+	printf("%d == %d\n", isalnum('a'), ft_isalnum('a'));
+	printf("%d == %d\n", isalnum('1'), ft_isalnum('1'));
+	printf("\n");
+
+	//isalpha
+	printf("isalpha :\n");
+	printf("%d == %d\n", isalpha('a'), ft_isalpha('a'));
+	printf("%d == %d\n", isalpha('1'), ft_isalpha('1'));
+	printf("\n");
+
+	//isascii
+	printf("isascii :\n");
+	printf("%d == %d\n", isascii(255), ft_isascii(255));
+	printf("%d == %d\n", isascii(0), ft_isascii(0));
+	printf("%d == %d\n", isascii(123), ft_isascii(123));
+	printf("%d == %d\n", isascii(1234), ft_isascii(1234));
+	printf("\n");
+
+	//isdigit
+	printf("isdigit :\n");
+	printf("%d == %d\n", isdigit(127), ft_isdigit(127));
+	printf("%d == %d\n", isdigit('1'), ft_isdigit('1'));
+	printf("%d == %d\n", isdigit('e'), ft_isdigit('e'));
+	printf("%d == %d\n", isdigit(21), ft_isdigit(21));
+	printf("\n");
+
+	//isprint
+	printf("isprint :\n");
+	printf("%d == %d\n", isprint(127), ft_isprint(127));
+	printf("%d == %d\n", isprint(360), ft_isprint(360));
+	printf("%d == %d\n", isprint('1'), ft_isprint('1'));
+	printf("%d == %d\n", isprint('e'), ft_isprint('e'));
+	printf("%d == %d\n", isprint(21), ft_isprint(21));
+	printf("\n");
+
+	//strlen
+	printf("strlen :\n");
+	printf("%lu == %lu\n", strlen("hohoho"), ft_strlen("hohoho"));
+	printf("%lu == %lu\n", strlen(""), ft_strlen(""));
+	//both seg fault printf("%lu\n", strlen(0x0), ft_strlen(0x0));
+	printf("\n");
+
+	//memset
+	printf("memset :\n");
+	int		array1[] = {54, 85, 20, 63, 21};
+	int		array2[] = {54, 85, 20, 63, 21};
+	size_t	size = sizeof(int) * 5;
+	int		length;
+	void	*ptr = ft_memset(array1, 0, size);
+	void	*ptr2 = memset(array2, 0, size);
+	//printf("size = %lu\n", size);
+	for( length=0; length<5; length++)
+		printf("%d ", array1[length]);
+	printf("== ");
+	for( length=0; length<5; length++)
+		printf("%d ", array2[length]);
+	ptr++;
+	ptr2++;
+	printf( "\n\n" );
+
+	//bzero
+	int array3 [] = {54, 85, 20, 63, 21};
+	int array4 [] = {54, 85, 20, 63, 21};
+	printf( "bzero :\n" );
+	ft_bzero(array3, size);
+	bzero(array4, size);
+	for( length=0; length<5; length++)
+		printf("%d ", array3[length]);
+	printf("== ");
+	for( length=0; length<5; length++)
+		printf("%d ", array4[length]);
+	printf("\n\n");
+
+	//memcpy
+	printf( "memcpy :\n" );
+	int	array[] = { 54, 85, 20, 63, 21 };
+	int	*copy;
+	int	*copy2;
+	copy = (int *) malloc(sizeof(int) * 5);
+	copy2 = (int *) malloc(sizeof(int) * 5);
+	memcpy(copy, array, size);
+	for(length=0; length<5; length++)
+		printf("%d ", copy[length]);
+	ft_memcpy(copy2, array, size);
+	printf("== ");
+	for(length=0; length<5; length++)
+		printf("%d ", copy2[length]);
+	printf( "\n\n" );
+	free(copy);
+	free(copy2);
+
+	//memmove
+
+	//strlcpy
+	printf("strlcpy :\n");
+	char *dst1 = malloc(sizeof(char) * 6);
+	char *dst2 = malloc(sizeof(char) * 6);
+	strlcpy(dst1, "ouioui", 6);
+	ft_strlcpy(dst2, "ouioui", 6);
+	printf("%s == %s\n\n", dst1, dst2);
+
+	//strlcat
+	printf("strlcat :\n");
+	char	*dst3 = malloc(sizeof(char) * 7);
+	dst3[0] = '1';
+	dst3[1] = '2';
+	dst3[2] = '3';
+	dst3[3] = 0;
+	char	*dst4 = malloc(sizeof(char) * 7);
+	dst4[0] = '1';
+	dst4[1] = '2';
+	dst4[2] = '3';
+	dst4[3] = 0;
+	ft_strlcat(dst3, "456", 7);
+	ft_strlcat(dst4, "456", 7);
+	printf("%s == %s\n\n", dst3, dst4);
+	free(dst3);
+	free(dst4);
+
+	//toupper
+	printf("toupper :\n");
+	printf("%c == %c\n", toupper('z'), ft_toupper('z'));
+	printf("%c == %c\n", toupper('Z'), ft_toupper('Z'));
+	printf("%c == %c\n", toupper('('), ft_toupper('('));
+	printf("%c == %c\n\n", toupper(0), ft_toupper(0));
+
+	//tolower
+	printf("tolower :\n");
+	printf("%c == %c\n", tolower('z'), ft_tolower('z'));
+	printf("%c == %c\n", tolower('['), ft_tolower('['));
+	printf("%c == %c\n", tolower(127), ft_tolower(127));
+	printf("%c == %c\n\n", tolower('Z'), ft_tolower('Z'));
+
+	//strchr
+	printf("strchr :\n");
+	printf("%s == %s\n\n", strchr("yzazo", 'z'), ft_strchr("yzazo", 'z'));
+
+	//strrch
+	printf("strrchr :\n");
+	printf("%s == %s\n\n", strrchr("yzazo", 'z'), ft_strrchr("yzazo", 'z'));
+
+	//strncmp
+	printf("strncmp :\n");
+	printf("%d\n\n", ft_strncmp("ouioui", "ouiouj", 5));
+
+	//memcmp
+	//strnstr
+	//atoi
+
+	//substr
+	//strjoin
+	//strtrim
+	//itoa
+	//split
+}
