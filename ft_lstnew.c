12c7f16a67caf27fd2458e902a0f860cc347e409
Tao <tao.spa2003@gmail.com>
2023-04-07 00:42:17 +0200

    ft_lstnew, not tested

---
+++ ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taospa <taospa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/06 22:13:17 by taospa            #+#    #+#             */
+/*   Updated: 2023/04/06 22:32:45 by taospa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	node->content = content;
+	node->next = 0x0;
+	return (node);
+}
