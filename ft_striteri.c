44356a50303e012b790a06b23fccfbdf0585b84e
Tao <tao.spa2003@gmail.com>
2023-04-10 19:26:47 +0200

    striteri with tests commented

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,17 +6,17 @@
 /*   By: taospa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 19:02:37 by taospa            #+#    #+#             */
-/*   Updated: 2023/04/10 19:23:11 by taospa           ###   ########.fr       */
+/*   Updated: 2023/04/10 19:26:24 by taospa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	function(unsigned int i, char *c)
-{
-	if (i % 2 && *c >= 'A' && *c <= 'Z')
-		*c = *c + 32;
-}
+// void	function(unsigned int i, char *c)
+// {
+// 	if (i % 2 && *c >= 'A' && *c <= 'Z')
+// 		*c = *c + 32;
+// }
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
@@ -30,19 +30,19 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	}
 }
 
-#include <stdio.h>
-int	main(void)
-{
-	char *str = malloc(sizeof(char) * 7);
-
-	str[0] = 'H';
-	str[1] = 'H';
-	str[2] = 'H';
-	str[3] = 'H';
-	str[4] = 'H';
-	str[5] = 'H';
-	str[6] = 0;
-	printf("str = %s\n", str);
-	ft_striteri(str, function);
-	printf("str = %s\n", str);
-}
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char *str = malloc(sizeof(char) * 7);
+//
+// 	str[0] = 'H';
+// 	str[1] = 'H';
+// 	str[2] = 'H';
+// 	str[3] = 'H';
+// 	str[4] = 'H';
+// 	str[5] = 'H';
+// 	str[6] = 0;
+// 	printf("str = %s\n", str);
+// 	ft_striteri(str, function);
+// 	printf("str = %s\n", str);
+// }
