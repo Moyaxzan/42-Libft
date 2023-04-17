8d57af8c842d47130d881156b3a4ca9f9f20a451
Tao <tao.spa2003@gmail.com>
2023-04-17 17:17:00 +0200

    fix norm errors addback

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,20 +6,17 @@
 /*   By: tsaint-p <tsaint-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 14:26:00 by tsaint-p          #+#    #+#             */
-/*   Updated: 2023/04/15 14:40:54 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/04/17 17:15:39 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "libft.h"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *last;
+	t_list	*last;
 
 	last = ft_lstlast(*lst);
 	if (lst && new)
-	{
 		last->next = new;
-	}
 }
