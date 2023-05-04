497794eafd59d7b8c6944598fe021cd591f306cb
Tao Saint paul amourdam <tsaint-p@e1r9p6.clusters.42paris.fr>
2023-05-04 19:55:25 +0200

    memchr : fix error

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: tsaint-p <tsaint-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 20:31:40 by taospa            #+#    #+#             */
-/*   Updated: 2023/05/04 19:02:54 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/05/04 19:51:05 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned long int	i;
-	char				*mem_block;
+	unsigned char				*mem_block;
 
-	i = 0;
-	mem_block = (char *) s;
-	while (i < n)
+	mem_block = (unsigned char *) s;
+	while (n--)
 	{
-		if (*mem_block == c)
+		if (*mem_block == (unsigned char)c)
 			return (mem_block);
 		mem_block++;
-		i++;
 	}
 	return (0x0);
 }
