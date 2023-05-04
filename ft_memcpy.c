f07728cfbe4049a98afa1c1e491c2ea96baf0e49
Tao Saint paul amourdam <tsaint-p@e1r9p6.clusters.42paris.fr>
2023-05-04 19:46:07 +0200

    memcpy : fix return when src NULL and size 0

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tsaint-p <tsaint-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 23:59:24 by marvin            #+#    #+#             */
-/*   Updated: 2023/05/04 19:06:54 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/05/04 19:45:02 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,12 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	long unsigned int	i;
 	unsigned char		*destination;
 	const unsigned char	*source;
 
-	destination = dest;
-	source = src;
-	if (!destination || !source)
-		return (0);
-	i = 0;
-	while (i < n)
-	{
-		destination[i] = source[i];
-		i++;
-	}
+	destination = (unsigned char *)dest;
+	source = (const unsigned char *)src;
+	while (n--)
+		*destination++ = *source++;
 	return (dest);
 }
