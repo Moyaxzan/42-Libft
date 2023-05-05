9739ef14bf8efc5cf52972340e5ac8bb1d8de1d5
Tao <tao.spa2003@gmail.com>
2023-05-05 11:33:19 +0200

    memcpy tests of NULL params fixed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tsaint-p <tsaint-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 23:59:24 by marvin            #+#    #+#             */
-/*   Updated: 2023/05/04 19:45:02 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/05/05 11:31:44 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	destination = (unsigned char *)dest;
 	source = (const unsigned char *)src;
+	if (!dest)
+		return (0x0);
+	if (!src)
+		return (dest);
 	while (n--)
 		*destination++ = *source++;
 	return (dest);
